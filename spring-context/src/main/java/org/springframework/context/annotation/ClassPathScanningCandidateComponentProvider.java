bb3f0c4cd0c132e9ddfb6e353e3b8bf0a1311f87
luowenzhi <luowenzhi@bigo.sg>
2022-01-23 20:30:03 +0800

    add comment

--- spring-context/src/main/java/org/springframework/context/annotation/ClassPathScanningCandidateComponentProvider.java
+++ spring-context/src/main/java/org/springframework/context/annotation/ClassPathScanningCandidateComponentProvider.java
@@ -193,6 +193,9 @@ public class ClassPathScanningCandidateComponentProvider implements EnvironmentC
 	}
 
 	/**
+	 * 注册过滤器
+	 * 带有@Component、@Repository、@Service、@Controller、@ManagedBean、@Named
+	 * 注解的类会被spring扫描到
 	 * Register the default filter for {@link Component @Component}.
 	 * <p>This will implicitly register all annotations that have the
 	 * {@link Component @Component} meta-annotation including the
